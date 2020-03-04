16a6b6982bb8c652d34796e76f5f17f7b47007ef
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-03-04 17:50:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -83,7 +83,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if (won?(board) && full?(board) && draw?(board))
+  if (won?(board) || full?(board) || draw?(board))
     return true
   end
   return false
